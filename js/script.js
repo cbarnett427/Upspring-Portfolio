f22fb81311e07f7cf10a19342eddabacb71f0a97
Clayton Barnett <Cbarnett427@live.com>
2022-08-17 11:16:04 -0500

    Update v0.1.2.3
    - Added hide navbar on scroll function
    - Changed navbar color for #fff

--- js/script.js
+++ js/script.js
@@ -107,3 +107,17 @@ window.addEventListener("scroll", reveal);
 
 // To check the scroll position on page load
 reveal();
+
+
+// Hide navbar on scroll
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-64px";
+  }
+  prevScrollpos = currentScrollPos;
+}
