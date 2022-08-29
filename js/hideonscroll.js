dfb6ad1d87a37a6cca671b432bd254e3c2576ceb
Clayton Barnett <Cbarnett427@live.com>
2022-08-29 11:41:51 -0500

    Add files via upload

--- js/hideonscroll.js
+++ js/hideonscroll.js
@@ -1,8 +1,24 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-let navBar = document.querySelector('nav')
-let prevScrollPos = window.pageYOffset
-window.addEventListener('scroll', () => {
-  let currentScrollPos = window.pageYOffset
-  prevScrollPos > currentScrollPos ? navBar.style.top = '0' : navBar.style.top = '-64px'
-  prevScrollPos = currentScrollPos
-})
+// let navBar = document.querySelector('nav')
+// let prevScrollPos = window.pageYOffset
+// window.addEventListener('scroll', () => {
+//   let currentScrollPos = window.pageYOffset
+//   prevScrollPos > currentScrollPos ? navBar.style.top = '0' : navBar.style.top = '-64px'
+//   prevScrollPos = currentScrollPos
+// })
+
+// Enable hidden nav bar
+{
+  const nav = document.querySelector(".navbar");
+  let lastScrollY = window.scrollY;
+
+  window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+      nav.classList.add("nav--hidden");
+    } else {
+      nav.classList.remove("nav--hidden");
+    }
+
+    lastScrollY = window.scrollY;
+  });
+}
